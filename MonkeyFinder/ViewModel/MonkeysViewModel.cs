c50cee5edc569a1a56cea8eb0aa09696b44f7bbd
Kevin Klose <kevin.klose@teamdynamix.com>
2023-11-15 13:47:36 -0500

    commit

---
+++ MonkeyFinder/ViewModel/MonkeysViewModel.cs
@@ -0,0 +1,22 @@
+﻿using MonkeyFinder.Services;
+
+namespace MonkeyFinder.ViewModel;
+
+public partial class MonkeysViewModel : BaseViewModel
+{
+  public MonkeysViewModel()
+  {
+  }
+
+  [RelayCommand]
+  async Task GoToDetails(Monkey monkey)
+  {
+    if (monkey == null)
+      return;
+
+    await Shell.Current.GoToAsync(nameof(DetailsPage), true, new Dictionary<string, object>
+    {
+        {"Monkey", monkey }
+    });
+  }
+}
